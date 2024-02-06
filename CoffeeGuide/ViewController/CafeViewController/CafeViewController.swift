314bae5e29de92bd7d17e06ade2de142f738dcfc
Jan Szala <janszala1993@gmail.com>
2024-02-06 16:47:01 +0100

    Resolved issue with cells not showing up on the CafeViewController. Created simple CafeCell. Added FlowLayout.

--- CoffeeGuide/ViewController/CafeViewController/CafeViewController.swift
+++ CoffeeGuide/ViewController/CafeViewController/CafeViewController.swift
@@ -29,31 +29,50 @@ internal class CafeViewController: UIViewController {
         super.viewDidLoad()
         
         setupCollectionView()
+        setupCollectionViewFlowLayout()
         collectionView.reloadData()
     }
     
     private func setupCollectionView() {
-        collectionView.register(CafeCollectionViewCell.self, forCellWithReuseIdentifier: CafeCollectionViewCell.identifier)
+        let cafeCellNib = UINib(nibName: "CafeCollectionViewCell", bundle: nil)
+        collectionView.register(cafeCellNib, forCellWithReuseIdentifier: "CafeCollectionViewCell")
         
         collectionView.delegate = self
         collectionView.dataSource = self
     }
+    
+    private func setupCollectionViewFlowLayout() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        collectionView.setCollectionViewLayout(layout, animated: true)
+    }
 }
 
-extension CafeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension CafeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return 50
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CafeCollectionViewCell.identifier,
-                                                          for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CafeCollectionViewCell", for: indexPath)
+        
         return cell
     }
-    
+        
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-       return CGSize(width: 100.0, height: 100.0)
+//        let flowayout = collectionViewLayout as? UICollectionViewFlowLayout
+//        let space: CGFloat = (flowayout?.minimumInteritemSpacing ?? 0.0) + (flowayout?.sectionInset.left ?? 0.0) + (flowayout?.sectionInset.right ?? 0.0)
+//        let size:CGFloat = (collectionView.frame.size.width - space) / 2.0
+//        return CGSize(width: size, height: size)
+        
+        return CGSize(width: 180, height: 180)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+           return UIEdgeInsets(top: 2, left: 10, bottom: 0, right: 10)
+        }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
--- CoffeeGuide/Views/Cells/CafeCollectionViewCell/CafeCollectionViewCell.swift
+++ CoffeeGuide/Views/Cells/CafeCollectionViewCell/CafeCollectionViewCell.swift
@@ -2,18 +2,13 @@
 //  CafeCollectionViewCell.swift
 //  CoffeeGuide
 //
-//  Created by GLaDOS on 05/02/2024.
+//  Created by GLaDOS on 06/02/2024.
 //
 
 import UIKit
 
 class CafeCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var cafeCollectionViewCell: UICollectionViewCell!
-    
-    static let identifier = "CafeCollectionViewCell"
-
     override func awakeFromNib() {
         super.awakeFromNib()
-        print("The cell has been loaded")
     }
 }
